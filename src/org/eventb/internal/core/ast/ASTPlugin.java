158d47755bacfe4451ad9789127e23582ce4e268
dgelessus <dgelessus@users.noreply.github.com>
2023-10-04 17:51:45 +0200

    Reformat build.gradle and generated code consistently with tabs
    To match the original Rodin code.

--- src/org/eventb/internal/core/ast/ASTPlugin.java
+++ src/org/eventb/internal/core/ast/ASTPlugin.java
@@ -1,9 +1,6 @@
+package org.eventb.internal.core.ast;
 
-  package org.eventb.internal.core.ast;
-
-  public class ASTPlugin {
-
-  public static String PLUGIN_ID = "who.cares";
-  public static void log(Object who, Object cares) {}
-
-  }
+public class ASTPlugin {
+	public static String PLUGIN_ID = "who.cares";
+	public static void log(Object who, Object cares) {}
+}
